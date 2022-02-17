be633e67a82b5e6da54e1e96d5bcafd288cbfa23
johljohl <97534501+johljohl@users.noreply.github.com>
2022-02-17 12:08:30 +0100

    Game_over

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
 const mario = document.getElementById("mario");
+const goomba = document.getElementById("goomba");
+const score = document.getElementById("score");
 
 
 function jump () {
     mario.classList.add("jump");
     setTimeout(() => {
         mario.classList.remove("jump");
-    }, 250);
+    }, 500);
 
 }
 document.addEventListener("keypress", function (event) {
+    if (!mario.classList.contains("jump")) {
     jump();
-}); 
+    }
+});
+    
+setInterval(() => {
+    score.innerText++;
+    const marioTop = parseInt(window.getComputedStyle(mario)
+    .getPropertyValue("top"));
+    const goombaLeft = parseInt(window.getComputedStyle(goomba)
+    .getPropertyValue("left"));
+    
+    if (goombaLeft < 0) {
+        goomba.style.display = "none";
+    } else {
+        goomba.style.display = "";
+    }
+
+    if ( goombaLeft < 50 && goombaLeft > 0 && marioTop > 150 ) {  
+        alert("Game Over, man game over!! Your score was: " + score.innerText + "\n\nPlay Again");
+        location.reload();
+        
+} 
+
+}, 50);
